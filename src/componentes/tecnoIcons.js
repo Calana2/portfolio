a5bbb27579e69414639fef05e0dea7848961c22e
Calana2 <Protoncala@proton.me>
2024-05-11 13:24:15 -0400

    1.0fixed2

--- src/componentes/tecnoIcons.js
+++ src/componentes/tecnoIcons.js
@@ -8,13 +8,13 @@ export default function TecnoIcons(){
            <div className="flex gap-2 absolute" 
             onMouseOver={()=>setResize(true)}
             onMouseOut={()=>setResize(false)}>
-            <SvgLink src={`${!resize ? '../../portfolio/typescript.svg' : '../../typescript64.svg'}`} alt="typescript" link="https://typescriptlang.org"
+            <SvgLink src={`${!resize ? '../../portfolio/typescript.svg' : '../../portfolio/typescript64.svg'}`} alt="typescript" link="https://typescriptlang.org"
             /> 
-            <SvgLink src={`${!resize ? '../../portfolio/react.svg' : '../../react64.svg'}`} alt="react" link="https://react.dev"
+            <SvgLink src={`${!resize ? '../../portfolio/react.svg' : '../../portfolio/react64.svg'}`} alt="react" link="https://react.dev"
             /> 
-            <SvgLink src={`${!resize ? '../../portfolio/nextjs.svg' : '../../nextjs64.svg'}`} alt="next" link="https://nextjs.org"
+            <SvgLink src={`${!resize ? '../../portfolio/nextjs.svg' : '../../portfolio/nextjs64.svg'}`} alt="next" link="https://nextjs.org"
             /> 
-            <SvgLink src={`${!resize ? '../../portfolio/tailwindcss.svg' : '../../tailwindcss64.svg'}`} alt="tailwind" link="https://tailwindcomponents.com"
+            <SvgLink src={`${!resize ? '../../portfolio/tailwindcss.svg' : '../../portfolio/tailwindcss64.svg'}`} alt="tailwind" link="https://tailwindcomponents.com"
             /> 
            </div>
    );
