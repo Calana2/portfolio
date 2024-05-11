93b3d6a2a9209bb4862505cddb0c22cfa65ae7dc
Calana2 <Protoncala@proton.me>
2024-05-11 11:55:43 -0400

    1.0

--- src/componentes/aboutMeParagraph.js
+++ src/componentes/aboutMeParagraph.js
@@ -2,10 +2,10 @@ import './aboutMeParagraph.css'
 
 export default function AboutMeParagraph(){
  return(
-  <div className="w-1/2 h-1/3 flex justify-center items-center flex-col break-words gap-2 aparecer">
+  <div className="md:w-1/2 md:h-1/3 h-1/4 width80P flex justify-center items-center flex-col break-words gap-2 aparecer">
  
   {/* TECNOLOGIAS */}
-   <p className='text-lg font-normal text-white text-left'>Fullstack web developer, experience with React frameworks such as
+   <p className='md:text-lg font-normal text-white text-left'>Fullstack web developer, experience with React frameworks such as
    <span className="text-red-500"> NextJs </span>
    , 
    <span className="text-blue-500"> Create-React-App </span>
@@ -13,12 +13,12 @@ export default function AboutMeParagraph(){
    <span className="text-purple-500"> Gatsby</span>.</p>
 
   {/* OBJETIVOS */}
-   <p className='text-lg font-normal text-white text-left'>
+   <p className='md:text-lg font-normal text-white text-left'>
     <span className="text-green-500">Open to work </span>
    as a remote developer, I accept freelance assignments. I would also be interested in becoming part of a company.</p>
 
   {/* IDIOMAS */}
-   <p className=' text-lg font-normal text-white text-left'>I am a native 
+   <p className=' md:text-lg font-normal text-white text-left'>I am a native 
     <span className="text-yellow-500"> Spa</span> 
     <span className="text-orange-500">ni</span>
     <span className="text-red-500">sh </span> 
@@ -32,7 +32,7 @@ export default function AboutMeParagraph(){
    , interested in their culture.</p>
    
   {/* HOBBY */}
-   <p className=' text-lg font-normal text-white text-left'>I like to read, philosophy and history are subjects that I am passionate about, I practice calisthenics and I have an interest in drawing and graphic design.</p>
+   <p className=' md:text-lg font-normal text-white text-left'>I like to read, philosophy and history are subjects that I am passionate about, I practice calisthenics and I have an interest in drawing and graphic design.</p>
    </div>
  ); 
 }
