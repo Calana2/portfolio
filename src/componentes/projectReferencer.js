4db517c425d40dd49c3ecfe6b8254af7b5696749
Calana2 <Protoncala@proton.me>
2024-08-01 17:32:44 -0400

    .

--- src/componentes/projectReferencer.js
+++ src/componentes/projectReferencer.js
@@ -5,8 +5,8 @@ export default function ProjectReferencer() {
       <ul className="list-disc pl grid gap-2">
         <li><a className="hover:text-green-500" href="https://github.com/Calana2/portfolio" alt="portfolio">This portfolio</a></li>
         <li><a className="hover:text-green-500" href="https://github.com/Calana2/Blog" alt="portfolio">The Blog</a></li>
+        <li><a className="hover:text-green-500" href="https://github.com/Calana2/cleverchat" alt="portfolio">Chat App</a></li>
         <li><a className="hover:text-green-500" href="https://github.com/Calana2/Imageboard" alt="portfolio">A ImageBoard</a></li>
-        <li><a className="hover:text-green-500" href="https://github.com/Calana2/ecomerceDemo" alt="portfolio">E-Commerce Template</a></li>
       </ul>
     </div>
   )
