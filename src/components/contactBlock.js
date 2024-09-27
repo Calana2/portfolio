fb3223ed7a2780118c620f66cb6c4976201085aa
Calana2 <Protoncala@proton.me>
2024-09-26 21:30:41 -0400

    linkedin changed

--- src/components/contactBlock.js
+++ src/components/contactBlock.js
@@ -6,7 +6,7 @@ export default function ContactBlock(){
   <div className="text-3xl font-bold flex items-center gap-2 font-serif italic"
   >
    <p className="text-white">Contact me:</p>
-   <SvgLink src='/linkedin.svg' alt="linkedin" link="https://cu.linkedin.com/in/kevin-cala%C3%B1a-52647a294"/>
+   <SvgLink src='/linkedin.svg' alt="linkedin" link="www.linkedin.com/in/kevin-calaña-castellón-998a9a32b"/>
    <SvgLink src='/github.svg' alt="github" link="https://github.com/Calana2"/>
    <SvgEmail  src='/protonmail.svg' alt="protonmail" email="kalcast00@proton.me"/>
   </div>
--- src/components/projectReferencer.js
+++ src/components/projectReferencer.js
@@ -1,6 +1,6 @@
 export default function ProjectReferencer() {
   return (
-    <div className="grid gap-2">
+    <div className="grid gap-2 aparecer">
       <span className="font-bold">List of repositories:</span>
       <ul className="list-disc pl grid gap-2">
         <li><a className="hover:text-green-500" href="https://github.com/Calana2/portfolio" alt="portfolio">Portfolio</a></li>
